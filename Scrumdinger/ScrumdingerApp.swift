5c567544d85149c7f6f519180769c04ebf5a216c
Mikhail Danilin <mihaildv@yahoo.com>
2023-05-05 14:51:46 +0300

    first commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Михаил Данилин on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
